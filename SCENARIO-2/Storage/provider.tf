a97406a66861c6e2474728214f5cf24fc2ccede2
gulamd <gulamrasool78655@gmail.com>
2021-07-26 09:30:04 +0530

    Create provider.tf

---
+++ SCENARIO-2/Storage/provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "${var.subscription-id}"
+  client_id       = "${var.client-id}"
+  client_secret   = "${var.client-secret}"
+  tenant_id       = "${var.tenant-id}"
+}
