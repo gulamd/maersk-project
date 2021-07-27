6061922182708d848b89b60dbe023e7e85627aea
gulamd <gulamrasool78655@gmail.com>
2021-07-27 17:49:57 +0530

    Create variable.tf

---
+++ SCENARIO-2/vms/variable.tf
@@ -0,0 +1,22 @@
+variable "subscription-id" {
+}
+variable "client-id" {
+}
+variable "client-secret" {
+}
+variable "tenant-id" {
+}
+variable "bastion-pubip-name" {
+}
+variable "flecqa-location" {
+}
+variable "bastion-nic-name" {
+}
+variable "flecqa-bastion-ip-name" {
+}
+variable "bastion-vm-name" {
+}
+variable "bastion-vmdisk-name" {
+}
+variable "bastion-username" {
+}
